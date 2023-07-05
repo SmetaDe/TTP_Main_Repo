f4445b392270dafd9c73fc1856f5b9f2317287a7
SmetaDe <smeta.deba@jjay.cuny.edu>
2023-07-05 18:56:31 -0400

    Project1

---
+++ DOM/scripts/index.js
@@ -0,0 +1,8 @@
+const element= document.
+getElementByID("myElement");
+// console.log(element)
+myElement.textContent ="New Text added";
+
+const paragraph = document.
+getElementByID("paragraph");
+paragraph.innerHTML = "<strong>New Content</strong>";
